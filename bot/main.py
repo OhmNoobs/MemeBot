bcf1e8deefacb5b5f56a1a79a1bf7db168c6dee0
jm <jo@jonasmai.de>
2018-03-21 10:14:58 +0100

    a better mozartizer

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,12 @@
 import logging
 import os
+import random
+
+os.urandom(2)
 
 from telegram.ext import Updater, CommandHandler
 
+VERSION = "0.1"
 
 def hello(bot, update):
     update.message.reply_text(f'Hello {update.message.from_user.first_name}')
@@ -10,13 +14,33 @@ def hello(bot, update):
 
 def mozartize(bot, update, args):
     out = ""
-    for letter in ''.join(args):
-        out += 'mNm'
+    i = 0
+    for word in args:
+        if i != 0:
+            out += ' '
+        if fortune_is_willing():
+            mozartized_word = ''
+            for _ in word:
+                if fortune_is_willing():
+                    mozartized_word += 'n' if fortune_is_willing() else 'N'
+                else:
+                    mozartized_word += 'M' if fortune_is_willing() else 'm'
+            out += mozartized_word
+        else:
+            out += word
+        i += 1
     update.message.reply_text(out)
 
 
+def fortune_is_willing():
+    return bool(random.getrandbits(1))
+
 def aehxtend(bot, update, args):
     update.message.reply_text(f"Coming soon™️")
+
+
+def version(bot, update):
+    update.message.reply_text(VERSION)
 
 
 def main():
@@ -28,6 +52,7 @@ def main():
     
     updater = Updater(bot_token)
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
+    updater.dispatcher.add_handler(CommandHandler('version', version))
     updater.dispatcher.add_handler(CommandHandler('mozartize', mozartize, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('ähxtend', aehxtend, pass_args=True))
     updater.start_polling()
