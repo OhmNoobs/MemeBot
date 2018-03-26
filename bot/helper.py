e15589b2d70215e0551668e58b2fcc0b29155ee8
adiko <dikodam@googlemail.com>
2018-03-26 10:08:30 +0200

    random function now with percentage parameter (default = 50%)

--- bot/helper.py
+++ bot/helper.py
@@ -1,5 +1,5 @@
 import random
 
 
-def fortune_is_willing() -> bool:
-    return bool(random.getrandbits(1))
+def fortune_is_willing(percent=50) -> bool:
+    return random.randrange(0, 100) < percent
