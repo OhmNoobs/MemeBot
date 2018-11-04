65ac6cf3df10efad3368ca318400ce609363578f
jm <jo@jonasmai.de>
2018-11-04 11:28:31 +0100

    added error_logger and inline query

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,16 @@
 import logging
 import os
 import sys
+from uuid import uuid4
+
 import telegram
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 
 from functions import food_scraper
 from logging import handlers
 from functions.Mozartizer import Mozartizer
 from functions.Aehxtender import Aehxtender
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler
 from functions.Exmatriculator import exmatriculate
 from functions.joke import make_joke_about
 from helper import Sentence
@@ -40,6 +43,34 @@ def version(_, update) -> None:
     update.message.reply_text(VERSION)
 
 
+def error_logger(_, update, error) -> None:
+    log.warning('Update "%s" caused error "%s"', update, error)
+
+
+def inline_query(_, update):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    aehxtended = Aehxtender(Sentence(query)).get_aehxtended()
+    if not aehxtended:
+        aehxtended = 'äh'
+    mozartized = Mozartizer(Sentence(query)).mozartize()
+    if not mozartized:
+        mozartized = 'Mnmnmnnn'
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Ähxtend",
+            description="Ähxtends your sentence!",
+            input_message_content=InputTextMessageContent(aehxtended)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Mozartize",
+            description="Get a mumbled version of your input.",
+            input_message_content=InputTextMessageContent(mozartized))
+    ]
+    update.inline_query.answer(results)
+
+
 def main():
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
     updater.dispatcher.add_handler(CommandHandler('version', version))
@@ -48,6 +79,8 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('food', food))
     updater.dispatcher.add_handler(CommandHandler('joke', joke, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('exmatrikulieren', exmatriculate, pass_args=True))
+    updater.dispatcher.add_handler(InlineQueryHandler(inline_query))
+    updater.dispatcher.add_error_handler(error_logger)
     updater.start_polling()
     updater.idle()
 
