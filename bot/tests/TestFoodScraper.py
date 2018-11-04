8b7439f56c26d4164adfe82681de74e10f811ca8
jm <jo@jonasmai.de>
2018-11-04 12:24:53 +0100

    fixed wrong assumption about how reading fs paths on multiple platforms works

--- bot/tests/TestFoodScraper.py
+++ bot/tests/TestFoodScraper.py
@@ -12,24 +12,24 @@ class TestFoodScraper(unittest.TestCase):
         cached_results_paths = PATH_TO_RESOURCES.glob('parsed_food_page_*.txt')
         actual_results = self.fetch_actual_results(cached_pages_paths)
         expected_results = self.fetch_expected_results(cached_results_paths)
-        for expected_result, actual_result in zip(expected_results, actual_results):
-            self.assertEqual(expected_result, actual_result)
+        for i in range(1, len(actual_results)+1):
+            self.assertEqual(actual_results[i], expected_results[i])
 
     @staticmethod
     def fetch_actual_results(cached_pages_paths):
-        actual_results = []
+        actual_results = {}
         for path in cached_pages_paths:
             with io.open(str(path), mode="r", encoding="utf-8") as soup_file:
                 soup = soup_file.read().replace('\r', '').replace('\n', '')
                 meals = food_scraper.cook_meals(soup)
                 feast = food_scraper.dish_up(meals)
-                actual_results.append(feast)
+                actual_results[int(soup_file.name[-5:-4])] = feast
         return actual_results
 
     @staticmethod
     def fetch_expected_results(cached_results_paths):
-        expected_results = []
+        expected_results = {}
         for path in cached_results_paths:
             with io.open(str(path), mode="r", encoding="utf-8") as result_file:
-                expected_results.append(result_file.read())
+                expected_results[int(result_file.name[-5:-4])] = result_file.read()
         return expected_results
