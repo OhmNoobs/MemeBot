0a96fa814cfda237e20686c39881af15e618557f
hubwoop <woop@tuta.io>
2018-12-30 20:57:27 +0100

    refactored joke

--- bot/functions/joke.py
+++ bot/functions/joke.py
@@ -1,6 +1,6 @@
 import re
 import requests
-from typing import List
+from typing import List, Optional
 
 CHUCK_API = "https://api.chucknorris.io/jokes/random"
 CHUCK = re.compile("chuck", re.IGNORECASE)
@@ -8,23 +8,34 @@ NORRIS = re.compile("norris", re.IGNORECASE)
 
 
 def make_joke_about(args: List[str]) -> str:
+    chuck_joke = get_chuck_joke()
+    joke = individualize(args, chuck_joke)
+    return joke
+
+
+def get_chuck_joke() -> Optional[str]:
     try:
         response = requests.get(CHUCK_API)
-    except requests.exceptions.RequestException:
+        response = response.json()["value"]
+    except requests.exceptions.RequestException or KeyError:
         response = None
+    return response
 
-    if response and response.json()["value"]:
-        response = response.json()
-        joke = response["value"]  # type: str
-        if args:
-            if len(args) > 0:
-                joke = CHUCK.sub(args[0].capitalize(), joke)
-            if len(args) > 1:
-                joke = NORRIS.sub(args[1].capitalize(), joke)
-    else:
-        if args:
-            joke_target = ' '.join(args)
-            joke = f"Da hat {joke_target} nochmal glück gehabt. Gibt grad keine Witze."
-        else:
-            joke = "Kein Witz :("
+
+def individualize(args, joke):
+    if joke and args:
+        joke = replace_chuck_with_args(args, joke)
+    elif args and not joke:
+        joke_target = ' '.join(args)
+        joke = f"Da hat {joke_target} nochmal glück gehabt. Gibt grad keine Witze."
+    elif not args and not joke:
+        joke = "Kein Witz :("
+    return joke
+
+
+def replace_chuck_with_args(args, joke):
+    if len(args) > 0:
+        joke = CHUCK.sub(args[0].capitalize(), joke)
+    if len(args) > 1:
+        joke = NORRIS.sub(args[1].capitalize(), joke)
     return joke
